9175fd46096797931b8f85dd428ad4041a82d7d5
Jonathan Smith <jonathan@peoplesized.com>
2019-02-20 10:29:22 +0200

    first

---
+++ longrun/handler.js
@@ -0,0 +1,19 @@
+"use strict"
+
+module.exports = (context, callback) => {
+  const params = JSON.parse(context)
+  const delay_s = params['delay_s'] || -1
+
+  const start = new Date()
+  const end = start.setSeconds(start.getSeconds() + delay_s)
+
+  let count = 0
+  while (new Date < end) {
+    count += 1
+  }
+
+  callback(undefined, {
+    status: `done after ${delay_s} seconds`,
+    count: count,
+  });
+}
