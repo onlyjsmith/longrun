4a34eae58daad4fd8148b568658afdee4c53f441
Jonathan Smith <jonathan@peoplesized.com>
2019-02-22 09:47:25 +0200

    better, versioned

--- longrun/handler.js
+++ longrun/handler.js
@@ -1,8 +1,18 @@
 "use strict"
 
-module.exports = (context, callback) => {
-  const params = JSON.parse(context)
-  const delay_s = params['delay_s'] || 10
+const DEFAULT_DELAY_S = 10
+const MAX_DELAY_S     = 120
+
+module.exports = (context) => {
+  let delay_s = DEFAULT_DELAY_S
+  
+  if (context) {
+    const params = JSON.parse(context)
+    params_delay_s = params['delay_s']
+    if (typeof params_delay_s === 'string' && params_delay_s < MAX_DELAY_S) {
+      delay_s = params_delay_s
+    }
+  }
 
   const start = new Date()
   const end = start.setSeconds(start.getSeconds() + delay_s)
