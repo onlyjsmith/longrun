6a5bb916a6f53eed774c07704c2f524151f16959
Jonathan Smith <jonathan@peoplesized.com>
2019-06-18 16:50:08 +0200

    good checks

--- function/handler.js
+++ function/handler.js
@@ -4,6 +4,7 @@ const DEFAULT_DELAY_S = 3; // 10;
 const MAX_DELAY_S = 30;
 
 module.exports = (context, callback) => {
+  const ALLOWED_PARAMS = ['delay_s', 'force_error'];
   let delay_s = DEFAULT_DELAY_S;
   let params;
   let count = 0;
@@ -11,13 +12,23 @@ module.exports = (context, callback) => {
   try {
     if (context) {
       params = JSON.parse(context);
+      const param_keys = Object.keys(params);
+      param_keys.forEach(k => {
+        if (!ALLOWED_PARAMS.includes(k)) {
+          throw new Error(`Param '${k}' not allowed`)
+        }
+      });
       
-      // Check for delay_s param
+      // Checks for delay_s param
       const params_delay_s = params["delay_s"];
-      if (typeof params_delay_s === "number" && params_delay_s <= MAX_DELAY_S) {
-        delay_s = params_delay_s;
-      } else if (params_delay_s > MAX_DELAY_S) {
-        throw new Error(`Param 'delay_s' set longer than allowed maximum of ${MAX_DELAY_S} seconds`)
+      if (params_delay_s) {
+        if (typeof params_delay_s === "number" && params_delay_s <= MAX_DELAY_S) {
+          delay_s = params_delay_s;
+        } else if (params_delay_s > MAX_DELAY_S) {
+          throw new Error(`Param 'delay_s' set longer than allowed maximum of ${MAX_DELAY_S} seconds`);q
+        } else if (typeof params_delay_s !== "number") {
+          throw new Error(`Param 'delay_s' provided, but is not a number >= 0 and <= ${MAX_DELAY_S}`);
+        }
       }
       
       // Check for force_error param
@@ -37,6 +48,7 @@ module.exports = (context, callback) => {
       function_status: "error",
       result: {
         message: error.message,
+        params,
       }
     })
   }
