7829456fab55b1cc9fc9710064697010c32c6840
Jonathan Smith <jonathan@peoplesized.com>
2019-06-18 16:38:09 +0200

    good

--- function/handler.js
+++ function/handler.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const DEFAULT_DELAY_S = 10;
-const MAX_DELAY_S = 120;
+const DEFAULT_DELAY_S = 3; // 10;
+const MAX_DELAY_S = 30;
 
 module.exports = (context, callback) => {
   let delay_s = DEFAULT_DELAY_S;
@@ -11,9 +11,18 @@ module.exports = (context, callback) => {
   try {
     if (context) {
       params = JSON.parse(context);
+      
+      // Check for delay_s param
       const params_delay_s = params["delay_s"];
       if (typeof params_delay_s === "number" && params_delay_s < MAX_DELAY_S) {
         delay_s = params_delay_s;
+      } else if (params_delay_s >= MAX_DELAY_S) {
+        throw new Error(`Param 'delay_s' set longer than allowed maximum of ${MAX_DELAY_S} seconds`)
+      }
+      
+      // Check for force_error param
+      if (!!params['force_error']) {
+        throw new Error('Forcing throw by param')
       }
     }
 
@@ -27,7 +36,7 @@ module.exports = (context, callback) => {
     return callback(undefined, {
       function_status: "error",
       result: {
-        message: JSON.stringify(error),
+        message: error.message,
       }
     })
   }
