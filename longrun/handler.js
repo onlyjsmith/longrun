2f39259e8f4e8146cd02080ec260148f36b827cc
Jonathan Smith <jonathan@peoplesized.com>
2019-06-18 16:19:05 +0200

    better handler

--- longrun/handler.js
+++ longrun/handler.js
@@ -1,32 +1,43 @@
-"use strict"
+"use strict";
 
-const DEFAULT_DELAY_S = 10
-const MAX_DELAY_S     = 120
+const DEFAULT_DELAY_S = 10;
+const MAX_DELAY_S = 120;
 
 module.exports = (context, callback) => {
-  let delay_s = DEFAULT_DELAY_S
-  
-  if (context) {
-    const params = JSON.parse(context)
-    const params_delay_s = params['delay_s']
-    if (typeof params_delay_s === 'number' && params_delay_s < MAX_DELAY_S) {
-      delay_s = params_delay_s
+  let delay_s = DEFAULT_DELAY_S;
+  let params;
+  let count = 0;
+
+  try {
+    if (context) {
+      params = JSON.parse(context);
+      const params_delay_s = params["delay_s"];
+      if (typeof params_delay_s === "number" && params_delay_s < MAX_DELAY_S) {
+        delay_s = params_delay_s;
+      }
     }
+
+    const start = new Date();
+    const end = start.setSeconds(start.getSeconds() + delay_s);
+
+    while (new Date() < end) {
+      count = count + 1;
+    }
+  } catch (error) {
+    return callback(undefined, {
+      function_status: "error",
+      result: {
+        message: JSON.stringify(error),
+      }
+    })
   }
 
-  const start = new Date()
-  const end = start.setSeconds(start.getSeconds() + delay_s)
-
-  let count = 0
-  while (new Date < end) {
-    count += 1
-  }
-
-  callback(undefined, {
-    function_status: 'success',
+  return callback(undefined, {
+    function_status: "success",
     result: {
+      params,
       status: `done after ${delay_s} seconds`,
       count: count,
     }
   });
-}
+};
